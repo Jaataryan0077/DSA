8ca9e8ed3e42de53cad007407a2623f417c54966
Aryan Deshwal <deshwalaryan456@gmail.com>
2026-03-13 22:52:45 +0530

    added file

--- L-43/string_match.cpp
+++ L-43/string_match.cpp
@@ -7,7 +7,23 @@ int main(){
     string big="abcdaabceaabceaabdop";
     string small="aabceaabdo";
     vector<int>lps(small.size(),0);
-     
+      int pre=0,suf=1;
+    while(suf<small.size()){
+        if(small[pre]==small[suf]){
+            lps[suf]=pre+1;
+            pre++,suf++;
+        }
+        else{
+            if(pre==0){
+                lps[suf]=0;
+                suf++;
+            }
+            else{
+                pre=lps[pre-1];
+            }
+        }
+
+    }
     int first=0,second=0;
     while(second<small.size()&&first<big.size())
     {
