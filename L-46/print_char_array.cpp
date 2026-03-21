f1aa6d92435e3fd69f0dbbcb7f0a101d1018d0fc
Aryan Deshwal <deshwalaryan456@gmail.com>
2026-03-21 22:20:43 +0530

    added new file

---
+++ L-46/print_char_array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char arr[]="1234";
+    char *ptr =arr;
+    cout<<arr<<endl;
+    cout<<ptr<<endl;
+
+    cout<<(void*)arr<<endl;
+    cout<<(void*)ptr;
+}
+
